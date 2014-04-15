3756d77fb6dae373bf19bb31dc9d5a61abf28c14
Antonio Nikishaev <antonio@lelf.me>
2014-04-15 21:51:29 +0400

    Chuwabra
    Ignore-this: 55e4308d62e363c9c146f9eb0217d530
    
    darcs-hash:20140415175129-898ea-6e7e9a744356cf1ba37221df79f1c3d8aef5339d

--- Acme/Pointful.hs
+++ Acme/Pointful.hs
@@ -20,29 +20,36 @@
 
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators #-}
 
+
+module Acme.Pointful (Pointful,
+                      id,(.)) where
 
 import Prelude hiding (id,(.))
 import Control.Category
 import Data.List.Split
-import Debug.Trace
-
-data N a b = N [Integer]
-             deriving Show
 
 
-instance Num (N a b) where
-    fromInteger = N . (:[])
+data Chuwabra a b = Pointful [Integer]
+                    deriving Show
+
+type (+) = Chuwabra
+type Pointful = () + ((),())
+
+
+instance Num (Chuwabra a b) where
+    fromInteger = Pointful . (:[])
     abs    = undefined
     signum = undefined
     (+)    = undefined
     (*)    = undefined
 
 
-instance Category N where
-    id = N []
-    N a . N b = N $ a++b
+instance Category Chuwabra where
+    id = Pointful []
+    Pointful a . Pointful b = Pointful $ a++b
 
-instance Num (N a b) => Fractional (N a b) where
-    fromRational = N . map read . splitOn "." . show . fromRational
+instance Num (Chuwabra a b) => Fractional (Chuwabra a b) where
+    fromRational = Pointful . map read . splitOn "." . show . fromRational
 
